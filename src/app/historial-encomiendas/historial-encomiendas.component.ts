39c05ac0968d7194071b1e186a19297b2c90e46f
Francisco Valverde <julval_09@hotmail.com>
2024-06-17 17:41:48 -0500

    creacion del componente ver historial encomienda

---
+++ src/app/historial-encomiendas/historial-encomiendas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-encomiendas',
+  standalone: true,
+  imports: [],
+  templateUrl: './historial-encomiendas.component.html',
+  styleUrl: './historial-encomiendas.component.css'
+})
+export class HistorialEncomiendasComponent {
+
+}
