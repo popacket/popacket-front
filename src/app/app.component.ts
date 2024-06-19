f52be9a40723a6fc183c9fdc0d352e101de89948
Izpo17F <ponce.1714.frp@gmail.com>
2024-06-19 01:06:00 -0500

    Creacion de rama QuoteShipment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CotizadorComponent } from './cotizador/cotizador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CotizadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/cotizador/cotizador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cotizador',
+  standalone: true,
+  imports: [],
+  templateUrl: './cotizador.component.html',
+  styleUrl: './cotizador.component.css'
+})
+export class CotizadorComponent {
+
+}
