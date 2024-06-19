c5160caf8f1c6047f506a03c327271e10b28dada
Francisco Valverde <julval_09@hotmail.com>
2024-06-19 01:27:31 -0500

    Primer commit de la rama

--- src/app/historial-encomiendas/historial-encomiendas.component.ts
+++ src/app/historial-encomiendas/historial-encomiendas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-historial-encomiendas',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './historial-encomiendas.component.html',
   styleUrl: './historial-encomiendas.component.css'
 })
