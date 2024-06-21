b7f89a4de2cd4ed2049ce3746162d8a7c5b73ca5
Izpo17F <ponce.1714.frp@gmail.com>
2024-06-21 16:40:56 -0500

    Disenando front de sipo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { NavbarComponent } from './navbar/navbar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyShipmentsComponent } from './user/my-ActiveShipments/my-ActiveShipments.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent,MyShipmentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
@@ -12,6 +13,6 @@ export class UserComponent implements OnInit{
   constructor (){}
 
   ngOnInit(): void {
-    
+
   }
 }
