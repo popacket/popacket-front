949f1189ae074ef604c9fd88216d15bd53e5a35d
Izpo17F <ponce.1714.frp@gmail.com>
2024-06-20 23:47:21 -0500

    Äctualizacion de front

---
+++ src/app/pages/shipment/cotizador/cotizador.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShipmentService } from '../service/shipment.service';
+import {
+	ReactiveFormsModule,
+	FormBuilder,
+	FormGroup,
+	Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-cotizador',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './cotizador.component.html',
+  styleUrl: './cotizador.component.css'
+})
+export class CotizadorComponent {
+  "quote" : number = 0;
+  costForm: FormGroup;
+  private shipmentService = inject(ShipmentService);
+
+  constructor(private fb: FormBuilder) {
+    this.costForm = this.fb.group({
+			weight: ['', Validators.required],
+			serviceType: ['', Validators.required]
+		});
+  }
+  onSubmit(){
+    const formValue = this.costForm.value;
+    const weight = formValue.weight;
+    const serviceType = formValue.serviceType;
+    this.shipmentService.getQuote(weight,serviceType).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.quote = response;
+      },
+      error: (error) => {
+        console.error('Error al obtener la cotización', error);
+      }
+    }
+    )
+  }
+}
