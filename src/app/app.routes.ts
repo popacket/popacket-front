576b516c610985aaa9078660f304d353dd2a3b9a
Izpo17F <ponce.1714.frp@gmail.com>
2024-06-20 22:57:03 -0500

    nuevas clases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/user/login/login.component'
 import { RegisterComponent } from './pages/user/register/register.component'
 import { CotizadorComponent } from './pages/shipment/cotizador/cotizador.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 export const routes: Routes = [
+  {
+    path: 'navbar',
+    component: NavbarComponent
+  },
   {
     path: 'login',
     component: LoginComponent
@@ -12,7 +17,7 @@ export const routes: Routes = [
     path: 'register',
     component: RegisterComponent
   },
-    {
+  {
     path : 'cotizador',
     component : CotizadorComponent
   }
