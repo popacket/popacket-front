1124d17454688b66ad7d296e8ed7f81a32e2c01f
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-06-23 23:33:56 -0500

    AActualizacion de rama CancelShipment

--- src/app/pages/packages/services/package.service.ts
+++ src/app/pages/packages/services/package.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../../../env/env';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PackageService {
 
-  private baseURL = env
+  private baseURL = environment.baseUrl
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getListActiveShipment():Observable<Package[]>{
+    return this.httpClient.get<Package[]>("${baseURL}/")
+  }
 }
