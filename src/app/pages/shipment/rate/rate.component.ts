640a27fde9351aecd473e69088c9ded10debce86
PabloAsmad15 <pabloasmad15@gmail.com>
2024-06-30 15:24:00 -0500

    Actualizacion de la rama RateService

---
+++ src/app/pages/shipment/rate/rate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rate',
+  standalone: true,
+  imports: [],
+  templateUrl: './rate.component.html',
+  styleUrl: './rate.component.css'
+})
+export class RateComponent {
+
+}
