2f54e24e419faa7c0b06172f95dda2083bb23716
PabloAsmad15 <pabloasmad15@gmail.com>
2024-07-03 14:47:30 -0500

    Actualizacion de la rama SaveUserPreferences

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/user/login/login.component'
 import { RegisterComponent } from './pages/user/register/register.component'
 import { RegisterComponent as DeliveryPersonRegisterComponent} from './pages/delivery-person/register/register.component'
-import { HomeComponent } from './pages/home/home.component';
-
+import { HomeComponent } from './pages/home/home.component'
+import { PreferencesComponent  } from './pages/user/preferences/preferences.component';
 export const routes: Routes = [
   {
     path: 'login',
@@ -25,5 +25,9 @@ export const routes: Routes = [
   {
     path: 'rate', 
     component: RateComponent
+  },
+  {
+    path: 'preferences/:userId',
+    component: PreferencesComponent
   }
 ];
